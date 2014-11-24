cfe181e4ae1ee7d6c6d397077bfb786d95f3efac
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-11-24 09:11:06 -0500

    parametrize move_cursor

--- src/move_cursor.v
+++ src/move_cursor.v
@@ -1,6 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 // move_cursor: This module implements a simple UI for manually adjusting the
-// projector correction
+// projector correction via pressing the arrow keys, and selecting which
+// corner of the quadrilateral the user is manipulating via switch positions
+// 00 -> point 1, 01 -> point 2, 10 -> point 3, 11 -> point 4
+// all the adjustments can only happen when the override switch is pressed
+// inputs are xi_raw, yi_raw (obtained from accelerometer lut)
+// outputs are xi, yi
 ////////////////////////////////////////////////////////////////////////////////
 module move_cursor(input clk,
                 input up,
@@ -27,7 +32,15 @@ module move_cursor(input clk,
                 output reg[9:0] x4,
                 output reg[8:0] y4);
 
-param OVERRIDE = 1'b0;
+parameter OVERRIDE = 1'b0;
+
+parameter XSPEED = 1'd1;
+parameter YSPEED = 1'd1;
+
+// 640 x 480 screen
+parameter SCR_WIDTH = 10'd639;
+parameter SCR_HEIGHT = 9'd479;
+
 reg cur_state = ~OVERRIDE;
 
 always @(posedge clk) begin
@@ -43,60 +56,68 @@ always @(posedge clk) begin
         y4 <= y4_raw;
     end
     else if (override) begin
+
+        // point (x1, y1)
         if ((!switch0) && (!switch1)) begin
             if (down) begin
-                y1 <= (y1 < 472) ? (y1 + 8) : y1;
+                y1 <= (y1 <= SCR_HEIGHT-YSPEED) ? (y1 + YSPEED) : y1;
             end
             else if (up) begin
-                y1 <= (y1 > 8) ? (y1 - 8) : y1;
+                y1 <= (y1 >= YSPEED) ? (y1 - YSPEED) : y1;
             end
             else if (left) begin
-                x1 <= (x1 > 8) ? (x1 - 8) : x1;
+                x1 <= (x1 >= XSPEED) ? (x1 - XSPEED) : x1;
             end
             else if (right) begin
-                x1 <= (x1 < 632) ? (x1 + 8) : x1;
+                x1 <= (x1 <= SCR_WIDTH-XSPEED) ? (x1 + XSPEED) : x1;
             end
         end
+
+        // point (x2, y2)
         else if ((switch0) && (!switch1)) begin
             if (down) begin
-                y2 <= (y2 < 472) ? (y2 + 8) : y2;
+                y2 <= (y2 <= SCR_HEIGHT-YSPEED) ? (y2 + YSPEED) : y2;
             end
             else if (up) begin
-                y2 <= (y2 > 8) ? (y2 - 8) : y2;
+                y2 <= (y2 >= YSPEED) ? (y2 - YSPEED) : y2;
             end
             else if (left) begin
-                x2 <= (x2 > 8) ? (x2 - 8) : x2;
+                x2 <= (x2 >= XSPEED) ? (x2 - XSPEED) : x2;
             end
             else if (right) begin
-                x2 <= (x2 < 632) ? (x2 + 8) : x2;
+                x2 <= (x2 <= SCR_WIDTH-XSPEED) ? (x2 + XSPEED) : x2;
             end
         end
+
+        // point (x3, y3)
         else if ((!switch0) && (switch1)) begin
             if (down) begin
-                y3 <= (y3 < 472) ? (y3 + 8) : y3;
+                y3 <= (y3 <= SCR_HEIGHT-YSPEED) ? (y3 + YSPEED) : y3;
             end
             else if (up) begin
-                y3 <= (y3 > 8) ? (y3 - 8) : y3;
+                y3 <= (y3 >= YSPEED) ? (y3 - YSPEED) : y3;
             end
             else if (left) begin
-                x3 <= (x3 > 8) ? (x3 - 8) : x3;
+                x3 <= (x3 >= XSPEED) ? (x3 - XSPEED) : x3;
             end
             else if (right) begin
-                x3 <= (x3 < 632) ? (x3 + 8) : x3;
+                x3 <= (x3 <= SCR_WIDTH-XSPEED) ? (x3 + XSPEED) : x3;
             end
         end
+
+        // point (x4, y4)
         else if ((switch0) && (switch1)) begin
             if (down) begin
-                y4 <= (y4 < 472) ? (y4 + 8) : y4;
+                y4 <= (y4 <= SCR_HEIGHT-YSPEED) ? (y4 + YSPEED) : y4;
             end
             else if (up) begin
-                y4 <= (y4 > 8) ? (y4 - 8) : y4;
+                y4 <= (y4 >= YSPEED) ? (y4 - YSPEED) : y4;
             end
             else if (left) begin
-                x4 <= (x4 > 8) ? (x4 - 8) : x4;
+                x4 <= (x4 >= XSPEED) ? (x4 - XSPEED) : x4;
             end
             else if (right) begin
-                x4 <= (x4 < 632) ? (x4 + 8) : x4;
+                x4 <= (x4 <= SCR_WIDTH-XSPEED) ? (x4 + XSPEED) : x4;
             end
         end
     end
@@ -109,6 +130,7 @@ always @(posedge clk) begin
         y3 <= y3_raw;
         x4 <= x4_raw;
         y4 <= y4_raw;
+        cur_state <= ~OVERRIDE;
     end
 end
 
