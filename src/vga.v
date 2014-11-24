35bc07919fce712585655fc52203bb3aa8f80273
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-11-24 09:09:19 -0500

    vga: remove params.v references

--- src/vga.v
+++ src/vga.v
@@ -5,40 +5,50 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module vga(input vclock,
-            output reg ['LOG_HCOUNT:0] hcount,    // pixel number on current line
-            output reg ['LOG_VCOUNT:0] vcount,	 // line number
+            output reg [10:0] hcount,  // pixel number on current line
+            output reg [10:0] vcount,  // line number
             output reg vsync,hsync,blank);
 
-   // horizontal: 800 pixels total
-   // display 640 pixels per line
-   reg hblank,vblank;
-   wire hsyncon,hsyncoff,hreset,hblankon;
-   assign hblankon = (hcount == 'VGA_HBLANKON);
-   assign hsyncon = (hcount == 'VGA_HSYNCON);
-   assign hsyncoff = (hcount == 'VGA_HYSNCOFF);
-   assign hreset = (hcount == 'VGA_HRESET);
+// VGA (640x480) @ 60 Hz
+parameter VGA_HBLANKON  =  10'd639;
+parameter VGA_HSYNCON   =  10'd655;
+parameter VGA_HYSNCOFF  =  10'd751;
+parameter VGA_HRESET    =  10'd799;
+parameter VGA_VBLANKON  =  10'd479;
+parameter VGA_VSYNCON   =  10'd490;
+parameter VGA_VSYNCOFF  =  10'd492;
+parameter VGA_VRESET    =  10'd523;
 
-   // vertical: 524 lines total
-   // display 480 lines
-   wire vsyncon,vsyncoff,vreset,vblankon;
-   assign vblankon = hreset & (vcount == 'VGA_VBLANKON);
-   assign vsyncon = hreset & (vcount == 'VGA_VSYNCON);
-   assign vsyncoff = hreset & (vcount == 'VGA_VSYNCOFF);
-   assign vreset = hreset & (vcount == 'VGA_VRESET);
+// horizontal: 800 pixels total
+// display 640 pixels per line
+reg hblank,vblank;
+wire hsyncon,hsyncoff,hreset,hblankon;
+assign hblankon = (hcount == VGA_HBLANKON);
+assign hsyncon = (hcount == VGA_HSYNCON);
+assign hsyncoff = (hcount == VGA_HYSNCOFF);
+assign hreset = (hcount == VGA_HRESET);
 
-   // sync and blanking
-   wire next_hblank,next_vblank;
-   assign next_hblank = hreset ? 0 : hblankon ? 1 : hblank;
-   assign next_vblank = vreset ? 0 : vblankon ? 1 : vblank;
-   always @(posedge vclock) begin
-      hcount <= hreset ? 0 : hcount + 1;
-      hblank <= next_hblank;
-      hsync <= hsyncon ? 0 : hsyncoff ? 1 : hsync;  // active low
+// vertical: 524 lines total
+// display 480 lines
+wire vsyncon,vsyncoff,vreset,vblankon;
+assign vblankon = hreset & (vcount == VGA_VBLANKON);
+assign vsyncon = hreset & (vcount == VGA_VSYNCON);
+assign vsyncoff = hreset & (vcount == VGA_VSYNCOFF);
+assign vreset = hreset & (vcount == VGA_VRESET);
 
-      vcount <= hreset ? (vreset ? 0 : vcount + 1) : vcount;
-      vblank <= next_vblank;
-      vsync <= vsyncon ? 0 : vsyncoff ? 1 : vsync;  // active low
+// sync and blanking
+wire next_hblank,next_vblank;
+assign next_hblank = hreset ? 0 : hblankon ? 1 : hblank;
+assign next_vblank = vreset ? 0 : vblankon ? 1 : vblank;
+always @(posedge vclock) begin
+    hcount <= hreset ? 0 : hcount + 1;
+    hblank <= next_hblank;
+    hsync <= hsyncon ? 0 : hsyncoff ? 1 : hsync;  // active low
 
-      blank <= next_vblank | (next_hblank & ~hreset);
-   end
+    vcount <= hreset ? (vreset ? 0 : vcount + 1) : vcount;
+    vblank <= next_vblank;
+    vsync <= vsyncon ? 0 : vsyncoff ? 1 : vsync;  // active low
+
+    blank <= next_vblank | (next_hblank & ~hreset);
+end
 endmodule
