81658d5501e99cc7af30033502812f5a1b208297
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-11-24 11:54:24 -0500

    clean up test_quad

--- src/test_quad.jl
+++ src/test_quad.jl
@@ -1,42 +1,44 @@
 using Winston
 
-function test_quad(p::Array{Float64, 2})
+function test_quad(p::Array{Int64, 2})
     println(p)
-    p /= 8;
     x = zeros(640*480)
     y = zeros(640*480)
     a = zeros(9)
     denom = p[4,1]*(p[2,2] - p[3,2]) + p[2,1]*(p[3,2]-p[4,2]) + p[3,1]*(p[4,2] - p[2,2])
-    a[3] = p[1,1]*denom*15
-    a[6] = p[1,2]*denom*15
-    a[9] = 15*denom;
+    a[3] = p[1,1]*denom*1920
+    a[6] = p[1,2]*denom*1920
+    a[9] = 1920*denom
     a[7] = ((p[1,1] - p[4,1])*(p[2,2] - p[3,2]) + (p[1,2]-p[4,2])*(p[3,1] - p[2,1]))*3
     a[8] = ((p[1,1] - p[2,1])*(p[3,2] - p[4,2]) + (p[1,2]-p[2,2])*(p[4,1] - p[3,1]))*4
     a[2] = a[8]*p[2,1] + 4*(p[4,1]-p[1,1])*denom;
     a[1] = a[7]*p[4,1] + 3*(p[2,1]-p[1,1])*denom;
     a[4] = a[7]*p[4,2] + 3*(p[4,2]-p[1,2])*denom;
     a[5] = a[8]*p[2,2] + 4*(p[2,2]-p[1,2])*denom;
+    count = 1
+    for i=1:640
+        for j=1:480
+            x[count], y[count] = pixel_map(i, j, a)
+            count += 1
+        end
+    end
+    p = plot(x, y)
+    display(p)
     return a
 end
 
+function pixel_map(x,y, a::Array{Float64, 1})
+    x_out = (a[1]*x + a[2]*y + a[3])/(a[7]*x + a[8]*y + a[9]);
+    y_out = (a[4]*x + a[5]*y + a[6])/(a[7]*x + a[8]*y + a[9]);
+    return x_out, y_out
+end
+
 function test_quad()
-    p1 = rand(1:80, 4, 1)
-    p2 = rand(1:60, 4, 1)
+    p1 = rand(1:640, 4, 1)
+    p2 = rand(1:480, 4, 1)
     p = [p1 p2]
     println(p)
-    x = zeros(640*480)
-    y = zeros(640*480)
-    a = zeros(9)
-    denom = p[4,1]*(p[2,2] - p[3,2]) + p[2,1]*(p[3,2]-p[4,2]) + p[3,1]*(p[4,2] - p[2,2])
-    a[3] = p[1,1]*denom*15
-    a[6] = p[1,2]*denom*15
-    a[9] = 15*denom;
-    a[7] = ((p[1,1] - p[4,1])*(p[2,2] - p[3,2]) + (p[1,2]-p[4,2])*(p[3,1] - p[2,1]))*3
-    a[8] = ((p[1,1] - p[2,1])*(p[3,2] - p[4,2]) + (p[1,2]-p[2,2])*(p[4,1] - p[3,1]))*4
-    a[2] = a[8]*p[2,1] + 4*(p[4,1]-p[1,1])*denom;
-    a[1] = a[7]*p[4,1] + 3*(p[2,1]-p[1,1])*denom;
-    a[4] = a[7]*p[4,2] + 3*(p[4,2]-p[1,2])*denom;
-    a[5] = a[8]*p[2,2] + 4*(p[2,2]-p[1,2])*denom;
+    a = test_quad(p)
     return a
 end
 
