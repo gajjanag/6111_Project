e672f0af6f91485bd22c7a0506b5e8a667851051
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-12-02 18:32:52 -0500

    slow_clk: bugfix

--- src/slow_clk.v
+++ src/slow_clk.v
@@ -1,15 +1,16 @@
 // this module generates a slow clock (e.g 1 hz signal)
 module slow_clk(input clk,
-                output reg slow_clk);
+                output slow_clk);
 
 reg[31:0] count = 0;
-slow_clk = 0;
+reg slow_clk_reg = 0;
 parameter JIFFIES = 27'd100_000_000;
 always @(posedge clk) begin
     count <= (count < JIFFIES) ? count + 1 : 0;
     if (count == 0) begin
-        slow_clk <= ~slow_clk;
+        slow_clk_reg <= ~slow_clk_reg;
     end
 end
+assign slow_clk = slow_clk_reg;
 endmodule
 
