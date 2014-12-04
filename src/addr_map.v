16f038a89e5fb116318a92581216e3a2ba009060
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-12-04 15:57:26 -0500

    addr_map: subtle bug

--- src/addr_map.v
+++ src/addr_map.v
@@ -13,5 +13,5 @@ module addr_map(input[9:0] hcount,
                 input[9:0] vcount,
                 output[16:0] addr);
 
-assign addr = (vcount << 7) + (vcount << 5) + (hcount >> 1);
+assign addr = (vcount[9:1] << 8) + (vcount[9:1] << 6) + (hcount >> 1);
 endmodule
