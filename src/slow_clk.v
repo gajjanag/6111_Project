a5f85f3a6e72ab2e558c7c66a1189d19f2402b3e
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-12-03 21:35:34 -0500

    slow_clk: update comment

--- src/slow_clk.v
+++ src/slow_clk.v
@@ -1,4 +1,11 @@
-// this module generates a slow clock (e.g 1 hz signal)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// this module generates a VERY SLOW clk by a simple counter
+// note: this method is NOT robust to timing issues, and for slowing
+// down/speeding up a clk by a reasonable multiple (e.g 2, 3), use DCM instead
+// to guarantee phase locking, elimination of most skew, etc
+// Here, the intent is only to generate a pulse with a time period of order of
+// seconds
+///////////////////////////////////////////////////////////////////////////////////////////////////
 module slow_clk(input clk,
                 output slow_clk);
 
