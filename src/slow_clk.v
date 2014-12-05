7545e59b8d4cd189209c150fd0371efa75b2dd2b
Ganesh Ajjanagadde <gajjanagadde@gmail.com>
2014-12-05 15:24:31 -0500

    slow_clk: use James style clock divider

--- src/slow_clk.v
+++ src/slow_clk.v
@@ -6,18 +6,21 @@
 // Here, the intent is only to generate a pulse with a time period of order of
 // seconds
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-module slow_clk(input clk,
-                output slow_clk);
+module slow_clk(input clk, output slow_clk);
+    parameter TICKS = 27'd49_999_999;
 
-reg[31:0] count = 0;
-reg slow_clk_reg = 0;
-parameter JIFFIES = 27'd100_000_000;
-always @(posedge clk) begin
-    count <= (count < JIFFIES) ? count + 1 : 0;
-    if (count == 0) begin
-        slow_clk_reg <= ~slow_clk_reg;
+    reg [31:0] count = 0;
+    reg sig_reg = 0;
+
+    always @(posedge clk) begin
+        if (count == TICKS) begin
+            // flip at half period
+            sig_reg <= ~sig_reg;
+            count <= 0;
+        end
+        else begin
+            count <= count + 1;
+        end
     end
-end
-assign slow_clk = slow_clk_reg;
+    assign slow_clk = sig_reg;
 endmodule
-
